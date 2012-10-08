f479ea96fda9258d867d467357896bc892746351
arijitvt <arijitnit06@gmail.com>
2012-10-08 16:35:39 -0400

    Variation of threads are added

--- NQausiLinearizability/src/core/Main.java
+++ NQausiLinearizability/src/core/Main.java
@@ -3,34 +3,40 @@ package core;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 	/*
-	 *  @Author Arijit
+	 * @Author Arijit
 	 */
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		long startTime = System.nanoTime();
 		Queue<Message> msgQ = new ConcurrentLinkedQueue<Message>();
-		
-		Producer producer = new Producer(msgQ);
-		Consumer consumer = new Consumer(msgQ);
-		
-		Thread producerThread = new Thread(producer);
-		Thread consumerThread = new Thread(consumer);
-		
-		
-		
-		producerThread.start();
-		consumerThread.start();		
-		try {
-			producerThread.join();
-			consumerThread.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		int noOfThreads = Integer
+				.parseInt(JOptionPane
+						.showInputDialog("Enter the number of producer and consumer threads"));
+		for (int i = 0; i < noOfThreads; ++i) {
+			Producer producer = new Producer(msgQ);
+			Consumer consumer = new Consumer(msgQ);
+
+			Thread producerThread = new Thread(producer);
+			Thread consumerThread = new Thread(consumer);
+
+			producerThread.start();
+			consumerThread.start();
+			try {
+				producerThread.join();
+				consumerThread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
-		
+
 		long endTime = System.nanoTime();
-		System.out.println("Total time taken : "+(long)((endTime - startTime)/1000000));
+		System.out.println("Total time taken in mili seconds : "
+				+ (long) ((endTime - startTime) / 1000000));
 	}
 }
-
