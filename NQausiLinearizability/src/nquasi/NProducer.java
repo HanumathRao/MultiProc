7016e15544fdd175beb01d3a789c78452ed2fbb3
Arijit <arijitnit06@gmail.com>
2012-10-11 00:24:05 -0400

    Adding more comments

--- NQausiLinearizability/src/nquasi/NProducer.java
+++ NQausiLinearizability/src/nquasi/NProducer.java
@@ -4,19 +4,29 @@ public class NProducer implements Runnable {
 	
 	private static final int MAX_ITERATION =50;
 	private NQuasiLinearizableQueueDynamic nQueue;
-	
+
+	/**
+	 * 
+	 * @param nQueue : Constructor
+	 */
 	public NProducer(NQuasiLinearizableQueueDynamic nQueue) {
 		this.nQueue = nQueue;
 	}
 	
+	@Override
 	public void run(){
 		for(int i =0 ; i < MAX_ITERATION ; ++i){
 			produce(i);
 		}
 	}
 	
+/**
+ * Enque operation is not synchronized, since in N-QuasiQueue many threads can push the data in the same node
+ * @param i : data to be pushed
+ */
 	public void produce(int i){
 			nQueue.enqueue(i);
+//			Notify all the consumer threads which are waiting when the threads are empty.
 			synchronized (nQueue) {
 				nQueue.notifyAll();
 			}
--- NQausiLinearizability/src/nquasi/Node.java
+++ NQausiLinearizability/src/nquasi/Node.java
@@ -1,19 +1,43 @@
 package nquasi;
-
+/**
+ * Single node consists of an array of size 1000, so that at a time if many threads 
+ * tries to enque to a location , all the values will be inserted in the same node but 
+ * in the differrent location of th array inside the node. 
+ * @author Arijit
+ *
+ */
 public class Node {
+//	Size of the cells inside the node
 	private final int SIZE = 1000;
+//	Array of the node
 	private int[] nodeArray = new int[SIZE];
+//	Index to control the array
 	private static int counter;
+//	Default constructor
 	public Node(){
 		counter = 0 ;
 	}
+	/**
+	 * @deprecated
+	 * @param num : Number to be added
+	 */
 	public Node(int num) {
 		this();
 		addElement(num);
 	}
+	
+	/**
+	 * 
+	 * @return : returns the node array
+	 */
 	public int[] getNodeArray() {
 		return nodeArray;
 	}
+	
+	/**
+	 * 
+	 * @param input: adds up element to the node
+	 */
 	
 	public void addElement(int input){
 		synchronized (nodeArray) {
@@ -22,6 +46,11 @@ public class Node {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param index: index of a perticular element inside the node
+	 * @return : the element which is in that perticular location
+	 */
 	public int getElement(int index){
 		return nodeArray[index];
 	}
