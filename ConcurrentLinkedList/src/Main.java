6eae2ee7a18cf85966c93a9c871e6571c2e7d92f
Arijit <arijitnit06@gmail.com>
2012-10-19 20:07:50 -0400

    Adding the lazylocking code

--- ConcurrentLinkedList/src/Main.java
+++ ConcurrentLinkedList/src/Main.java
@@ -12,10 +12,10 @@ public class Main {
 	}
 	
 	public static void main(String args[]){
-		FineGrainedConcLinkedList<Integer> list = new FineGrainedConcLinkedList<Integer>();
+		/*FineGrainedConcLinkedList<Integer> list = new FineGrainedConcLinkedList<Integer>();
 		for(int i = 0 ; i< 5;  ++i){
 			list.add(i+12);
 		}
-		Main.measureFineGrain(list);
+		Main.measureFineGrain(list);*/
 	}
 }
